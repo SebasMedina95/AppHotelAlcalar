0795763f8459f0f386e3a253867edbd3064e177b
Sebastian Medina <jsebastian19952011@gmail.com>
2024-07-19 10:53:56 -0500

    Primer Commit

---
+++ src/main/java/com/sebastian/springboot/hostal_alcalar/hostal_alcalar/HostalAlcalarApplication.java
@@ -0,0 +1,13 @@
+package com.sebastian.springboot.hostal_alcalar.hostal_alcalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostalAlcalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostalAlcalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastian/springboot/hostal_alcalar/hostal_alcalar/HostalAlcalarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.springboot.hostal_alcalar.hostal_alcalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalAlcalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
